3398573476d0807f1eb1d3470f714845f235e92a
Qamarian <me@qamarian.com>
2019-05-29 02:28:25 +0100

    Update aaaaaa.Main.go

--- aaaaaa.Main.go
+++ aaaaaa.Main.go
@@ -1,11 +1,16 @@
 package main
 
-/* This is the main and single most important component of this framework. It starts your app and can also be used to gracefully shutdown the app. */
+/* This is the main and single most important component of this framework.
+        It starts your app and can also be used to gracefully shutdown the app. */
 
 import "runtime"
 
 func main () {
-        // Continously check if global shutdown has been signalled: if yes, the whole app built on this framework will shutdown.
+        // All initialization functions registered with this component will be invoked.
+        sInvokeInitFuncs ()
+
+        // Continously check if global shutdown has been signalled:
+        // if yes, the whole app built on this framework will shutdown.
         for {
                 select {
                         case _, _ = <- dShutdownChannel_AAAAAA: return
@@ -14,12 +19,23 @@ func main () {
 
                 runtime.Gosched ()
         }
+
+        // All deinitialization functions registered with this component will be invoked.
+        sInvokeDnitFuncs ()
+}
+
+func iRegisterInitFunc (compID string, initFunc func (), precedentComp []string) {
+}
+
+func iRegisterDnitFunc (compID string, dnitFunc func (), precedentComp []string) {
 }
 
 func iShutdown_AAAAAA () { // To shutdown your app gracefully, this interface can be called.
         dShutdownChannel_AAAAAA <- true
 }
 
-var (
-        dShutdownChannel_AAAAAA chan bool = make (chan bool, 1) // This channel is private, and must not be used by other components.
-)
+func sInvokeInitFuncs () {}
+
+func sInvokeDnitFuncs () {}
+
+var dShutdownChannel_AAAAAA chan bool = make (chan bool, 1)
